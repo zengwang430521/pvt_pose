c2948dbf6021327f2e4ad3c20f7f03f4df9c7d3c
zengwang <zengwang430521@gmail.com>
2021-11-02 01:43:29 +0800

    'debug'

--- models/utils_mine.py
+++ models/utils_mine.py
@@ -2,7 +2,10 @@ import torch
 import torch.nn.functional as F
 import math
 import matplotlib.pyplot as plt
-from torch_cluster import fps
+try:
+    from torch_cluster import fps
+except (ImportError, ModuleNotFoundError):
+    print('no torch cluster')
 
 
 def get_grid_loc(B, H, W, device):
