c1b4199670131134c87509ed56456b1e6bffb95a
zengwang <zengwang430521@gmail.com>
2021-11-02 14:24:16 +0800

    debug

--- train.sh
+++ train.sh
@@ -7,10 +7,11 @@ spring.submit arun -p spring_scheduler --gres=gpu:8 --ntasks-per-node=8 --cpus-p
     -n6 --gpu --job-name=mesh --gpu-type 16gv100  -w SH-IDC1-10-198-6-245 \
     "
      python -u main.py --batch_size=24 --num_workers=5 --num_epochs=100 --summary_steps=100 \
-    --opt=adamw --lr=2.5e-4 --wd=0.01 --lr_drop=90  --img_res=224 --use_mc \
-    --model=mypvt3h2_densityc_small --dataset=mix1 --head_type=hiratt_hmr \
-    --name=denc_mix1_hiratt  --pretrain_from=data/pretrained/3h2_densityc_small.pth\
-    --val_dataset=3dpw --resume_from=logs/denc_mix1_hiratt/checkpoints/checkpoint_latest.pth
+    --opt=adamw --lr=1e-4 --wd=0.01 --lr_drop=90  --img_res=224 --use_mc \
+    --model=mypvt3h2_density0_small --dataset=mix1 --head_type=hiratt_hmr \
+    --name=den0_mix1_hiratt  --pretrain_from=data/pretrained/3h2_density0_small.pth\
+    --val_dataset=3dpw --resume_from=logs/den0_mix1_hiratt/checkpoints/checkpoint_latest.pth
+    --lam_smpl_beta=0.002
     "
 
 
@@ -25,7 +26,7 @@ srun -p pat_earth -x SH-IDC1-10-198-4-[100-103,116-119] \
 srun -p mm_human \
     --ntasks 8 --job-name=mesh --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=5 --kill-on-bad-exit=1 \
     python -u main.py --batch_size=32 --num_workers=5 --num_epochs=100 --summary_steps=100 \
-    --opt=adamw --lr=2.5e-4 --wd=0.01 --lr_drop=90  --img_res=224 --use_mc \
+    --opt=adamw --lr=1e-4 --wd=0.01 --lr_drop=90  --img_res=224 --use_mc \
      \
     --model=mypvt3h2_density0_small --dataset=mix1 --head_type=hiratt_hmr \
     --name=den0_mix1_hiratt  --pretrain_from=data/pretrained/3h2_density0_small.pth\
