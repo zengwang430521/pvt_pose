fa1df5c534afc3da063c6f0cce66cd7faf5cdb29
zengwang <zengwang430521@gmail.com>
2022-03-30 16:05:45 +0800

    debug

--- train.sh
+++ train.sh
@@ -34,6 +34,13 @@ srun -p pat_earth \
     \
     python -u main.py --batch_size=32 --num_workers=5 --num_epochs=60 --summary_steps=100 \
     --opt=adamw --lr=1e-4 --wd=0.01 --lr_drop=50  --img_res=224 --use_mc \
+    --model=tcformer_small --dataset=mix1 --head_type=hiratt_hmr3 \
+    --name=tc_mix1_att3_l4  --pretrain_from=data/pretrained/3h2_density0_small.pth\
+    --loss_type=4 --resume_from=logs/tc_mix1_att3_l4/checkpoints/checkpoint_latest.pth \
+    --lam_smpl_beta=0.002     --eval_freq=1  --val_dataset=3dpw
+
+    python -u main.py --batch_size=32 --num_workers=5 --num_epochs=60 --summary_steps=100 \
+    --opt=adamw --lr=1e-4 --wd=0.01 --lr_drop=50  --img_res=224 --use_mc \
     --model=tcformer_small --dataset=mix1 --head_type=mta_hmr \
     --name=tc_mix1_mta_l4  --pretrain_from=data/pretrained/3h2_density0_small.pth\
     --loss_type=4 --resume_from=logs/tc_mix1_mta_l4/checkpoints/checkpoint_latest.pth \
